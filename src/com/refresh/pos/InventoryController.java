27b40c4e27a17000747bc8d1dadde90181b8e9a4
teeravipark <teeravipark@gmail.com>
2013-10-02 13:26:29 +0700

    add InventoryController.java

---
+++ src/com/refresh/pos/InventoryController.java
@@ -0,0 +1,19 @@
+package com.refresh.pos;
+
+
+
+public class InventoryController {
+	private ItemFactory itemFactory;
+	private Inventory inventory;
+	
+	public InventoryController(Database database) {
+		inventory = new Inventory(database);
+		itemFactory = ItemFactory.getInstance();
+	}
+	
+	public boolean add(){
+		Item item = itemFactory.createItem("Apple", "885100422323222", 10.50);
+    	boolean success = inventory.addNewItem(item);
+    	return success;
+	}
+}
