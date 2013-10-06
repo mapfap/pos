8b9f9a29594579410900dccc48404d4f62080786
LostSeaWay <LostSeaWay@me.com>
2013-10-06 16:25:21 +0700

    AddActivity Add ClearButton Function

--- src/com/refresh/pos/AddActivity.java
+++ src/com/refresh/pos/AddActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class AddActivity extends Activity {
@@ -16,16 +17,36 @@ public class AddActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add);
 		super.onCreate(savedInstanceState);
+		final EditText itemName = (EditText) findViewById(R.id.nameTxt);
+		final EditText itemCost = (EditText) findViewById(R.id.costTxt);
+		final EditText itemPrice = (EditText) findViewById(R.id.priceTxt);
+		final EditText itemAmount = (EditText) findViewById(R.id.amountTxt);
+		final EditText itemDetail = (EditText) findViewById(R.id.detailTxt);
 		
-		final Button insertButton = (Button) findViewById(R.id.addButton);
-		final TextView itemName = (TextView) findViewById(R.id.nameTxt);
-		insertButton.setOnClickListener(new View.OnClickListener() {
+		final Button addButton = (Button) findViewById(R.id.addButton);
+		addButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-			Log.v(">>>",itemName.getText().toString());	
+			Log.v(">>>",itemName.getText().toString());
+			Log.v(">>>",itemCost.getText().toString());	
+			Log.v(">>>",itemPrice.getText().toString());	
+			Log.v(">>>",itemAmount.getText().toString());	
+			Log.v(">>>",itemDetail.getText().toString());	
 
 			}
 		});
-		
+		final Button clearButton = (Button) findViewById(R.id.clearButton);
+		clearButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				itemName.setText("");
+				itemCost.setText("");
+				itemPrice.setText("");
+				itemAmount.setText("");
+				itemDetail.setText("");
+				
+			}
+		});
 	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
