29f64af23cdc008a64e6fdf9d3eb645b0ba4189e
teeravipark <teeravipark@gmail.com>
2013-10-08 11:56:02 +0700

    added new button

--- gen/com/refresh/pos/R.java
+++ gen/com/refresh/pos/R.java
@@ -21,7 +21,13 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int btn_black=0x7f020000;
+        public static final int btn_blue=0x7f020001;
+        public static final int btn_green=0x7f020002;
+        public static final int btn_purple=0x7f020003;
+        public static final int btn_red=0x7f020004;
+        public static final int btn_yellow=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
     }
     public static final class id {
         public static final int ColBarcode=0x7f08000a;
@@ -75,11 +81,15 @@ public final class R {
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+
+
     
 
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
+
+
         
 
         Base application theme for API 11+. This theme completely replaces
@@ -93,10 +103,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f060001;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f060002;
+        public static final int ButtonText=0x7f060000;
     }
 }
