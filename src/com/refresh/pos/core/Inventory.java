f77ed86811f8c4db5ee33b8b929f4702e361a29d
teeravipark <teeravipark@gmail.com>
2013-10-09 09:25:34 +0700

    fixed ShowListActivity

--- src/com/refresh/pos/core/Inventory.java
+++ src/com/refresh/pos/core/Inventory.java
@@ -1,5 +1,13 @@
 package com.refresh.pos.core;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
 import com.refresh.pos.database.Dao;
 import com.refresh.pos.database.NoDaoSetException;
 
@@ -44,5 +52,20 @@ public class Inventory {
 		return productCatalog.addNewProduct(productFactory.createProduct(name, barcode, price));
 	}
 
+	// Show All Data
+	public ArrayList<HashMap<String, String>> SelectAllStock() {
+		ArrayList<HashMap<String, String>> stockList = new ArrayList<HashMap<String, String>>();
+		List<Product> productList = this.getProductCatalog().getAllProduct();
+		for (Product product : productList) {
+			HashMap<String, String> map = new HashMap<String, String>();
+	        map.put("_id", product.getId()+"");
+ 	        map.put("name", product.getName());
+ 	        map.put("barcode", product.getBarcode());
+ 	        map.put("sale_price", product.getSalePrice()+"");
+ 	        stockList.add(map);
+		}
+		return stockList;
+
+	}
 
 }
--- src/com/refresh/pos/ui/ShowListActivity.java
+++ src/com/refresh/pos/ui/ShowListActivity.java
@@ -1,24 +1,25 @@
 package com.refresh.pos.ui;
 
-import java.util.List;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import com.refresh.pos.R;
 import com.refresh.pos.core.Inventory;
-import com.refresh.pos.core.Product;
 import com.refresh.pos.database.NoDaoSetException;
 
 public class ShowListActivity extends Activity {
 
 	private Inventory inventory;
-	private List<Map<String, String>> stockList;
+//	private List<Map<String, String>> stockList;
+	ArrayList<HashMap<String, String>> stockList;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -32,11 +33,12 @@ public class ShowListActivity extends Activity {
 			e.printStackTrace();
 		}
 
-		List<Product> productList = inventory.getProductCatalog().getAllProduct();
-		Toast.makeText(ShowListActivity.this,productList.get(0).getName()+productList.get(0).getBarcode()+
-				productList.get(0).getId()+productList.get(0).getSalePrice(),
-				Toast.LENGTH_SHORT).show();
+//		List<Product> productList = inventory.getProductCatalog().getAllProduct();
+//		Toast.makeText(ShowListActivity.this,productList.get(0).getName()+productList.get(0).getBarcode()+
+//				productList.get(0).getId()+productList.get(0).getSalePrice(),
+//				Toast.LENGTH_SHORT).show();
 		
+		stockList = inventory.SelectAllStock();
 		
 //		for (Product product : productList) {
 //			Map<String, String> map = new HashMap<String, String>();
@@ -48,14 +50,14 @@ public class ShowListActivity extends Activity {
 //		}
 
 		// listView1
-//		ListView lisView1 = (ListView) findViewById(R.id.listView1);
-//
-//		SimpleAdapter sAdap;
-//		sAdap = new SimpleAdapter(ShowListActivity.this, stockList,
-//				R.layout.activity_column, new String[] { "_id", "name",
-//						"barcode","sale_price" }, new int[] { R.id.ColProductID,
-//						R.id.ColName, R.id.ColBarcode, R.id.ColSalePrice });
-//		lisView1.setAdapter(sAdap);
+		ListView lisView1 = (ListView) findViewById(R.id.listView1);
+
+		SimpleAdapter sAdap;
+		sAdap = new SimpleAdapter(ShowListActivity.this, stockList,
+				R.layout.activity_column, new String[] { "name",
+						"barcode","sale_price" }, new int[] { R.id.ColProductID,
+						R.id.ColName, R.id.ColBarcode });
+		lisView1.setAdapter(sAdap);
 
 		final Button addProductButton = (Button) findViewById(R.id.addNewProduct);
 
