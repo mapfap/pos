dfe8574d982b58b74a2d4ca193fbd36ca57d352b
teeravipark <teeravipark@gmail.com>
2013-10-30 12:43:51 +0700

    Add 2 activity

--- src/com/refresh/pos/ui/ShowInventoryActivity.java
+++ src/com/refresh/pos/ui/ShowInventoryActivity.java
@@ -7,8 +7,9 @@ import java.util.Map;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
@@ -19,16 +20,16 @@ import com.refresh.pos.core.Product;
 import com.refresh.pos.core.ProductCatalog;
 import com.refresh.pos.database.NoDaoSetException;
 
-public class ShowListActivity extends Activity {
+public class ShowInventoryActivity extends Activity {
 
 	private ListView lisView1;
 	private ProductCatalog productCatalog;
-	List<Map<String, String>> stockList;
+	List<Map<String, String>> inventoryList;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_showlist);
+		setContentView(R.layout.activity_productlist);
 
 		try {
 			productCatalog = Inventory.getInstance().getProductCatalog();
@@ -45,31 +46,39 @@ public class ShowListActivity extends Activity {
 
 		addProductButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				Intent newActivity = new Intent(ShowListActivity.this,AddActivity.class);
+				Intent newActivity = new Intent(ShowInventoryActivity.this,AddActivity.class);
 				startActivity(newActivity);
 			}
 		});
-		final Button refreshButton = (Button) findViewById(R.id.refreshButton);
-		refreshButton.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				showList();
-			}
-		});
+//		final Button refreshButton = (Button) findViewById(R.id.refreshButton);
+//		refreshButton.setOnClickListener(new View.OnClickListener() {
+//			
+//			@Override
+//			public void onClick(View v) {
+//				showList();
+//			}
+//		});
+		lisView1.setOnItemClickListener(new OnItemClickListener() {
+		      public void onItemClick(AdapterView<?> myAdapter, View myView, int position, long mylng) {
+		    	  String id = inventoryList.get(position).get("id").toString();
+		    	  Intent newActivity = new Intent(ShowInventoryActivity.this,ShowItemActivity.class);
+		    	  newActivity.putExtra("product_id", id);
+				  startActivity(newActivity);  	    	  
+		      }     
+      });
 
 	}
 	
 	private void showList() {
 		
-		stockList = new ArrayList<Map<String, String>>();
+		inventoryList = new ArrayList<Map<String, String>>();
 		List<Product> catalog = productCatalog.getAllProduct();
 		for(Product product : catalog) {
-			stockList.add(product.toMap());
+			inventoryList.add(product.toMap());
 		}
 		
 		SimpleAdapter sAdap;
-		sAdap = new SimpleAdapter(ShowListActivity.this, stockList,
+		sAdap = new SimpleAdapter(ShowInventoryActivity.this, inventoryList,
 				R.layout.activity_column, new String[] { "name",
 						"barcode"}, new int[] { R.id.ColProductID,
 						R.id.ColName});
