35371bb2ceb2eaf85f72941be3ba9e2e91141ea8
ixistic <ixistic@158.108.67.35>
2013-11-21 11:12:46 +0700

    remove useless comment.

--- src/com/refresh/pos/ui/AddProductLotActivity.java
+++ src/com/refresh/pos/ui/AddProductLotActivity.java
@@ -19,7 +19,6 @@ import com.refresh.pos.domain.ProductCatalog;
 import com.refresh.pos.domain.Stock;
 
 public class AddProductLotActivity extends Activity{
-//	private EditText itemBarcode;
 	private EditText costBox;
 	private EditText quantityBox;
 	private Stock stock;
@@ -28,36 +27,7 @@ public class AddProductLotActivity extends Activity{
 	private String id;
 	private ProductCatalog productCatalog;
 	private Product product;
-	
-//	private Inventory inventory;
-//	private double quantity;
-//
-//	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-//		//		Log.d("BARCODE", "BARCODE 'onActivityResult' Successfully.");
-//
-//		IntentResult scanningResult = IntentIntegrator.parseActivityResult(
-//				requestCode, resultCode, intent);
-//		//
-//		if (scanningResult != null) {
-//			String scanContent = scanningResult.getContents();
-//			String name = inventory.getProductCatalog().getProductByBarcode(scanContent).getName();
-//			if(name.equals("UNDEFINED")){
-//				Toast.makeText(StockAddActivity.this,
-//						"Can not find " + scanContent +" in database", Toast.LENGTH_SHORT)
-//						.show();
-//			}
-//			else{
-//				itemBarcode.setText(scanContent);
-//				itemName.setText(name);
-//			}
-//			//
-//		} else {
-//			Toast.makeText(StockAddActivity.this,
-//					"Failed to retrieve barcode." + resultCode, Toast.LENGTH_SHORT)
-//					.show();
-//		}
-//	}
-//
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -128,98 +98,7 @@ public class AddProductLotActivity extends Activity{
 			});
 	
 		}
-//		super.onCreate(savedInstanceState);
-//		setContentView(R.layout.activity_addproductlot);
-//		final EditText quantityTxt = (EditText) findViewById(R.id.quantityTxt);
-//		quantity = Double.parseDouble(quantityTxt.getText().toString());
-//		itemName = (EditText) findViewById(R.id.nameTxt);
-//		itemBarcode = (EditText) findViewById(R.id.barcodeTxt);
-//				itemBarcode.setEnabled(false);
-//				itemName.setEnabled(false);
-//		itemPrice = (EditText) findViewById(R.id.costTxt);
-//		final Button scanButton = (Button) findViewById(R.id.scanButton);
-//		scanButton.setOnClickListener(new View.OnClickListener() {
-//			@Override
-//			public void onClick(View v) {
-//				IntentIntegrator scanIntegrator = new IntentIntegrator(StockAddActivity.this);
-//				scanIntegrator.initiateScan();
-//
-//
-//			}
-//		}
-//				);
-//		final Button plusButton = (Button) findViewById(R.id.plus);
-//		plusButton.setOnClickListener(new View.OnClickListener() {
-//
-//			@Override
-//			public void onClick(View v) {
-//				quantity = Integer.parseInt(quantityTxt.getText().toString());
-//				quantity++;
-//				quantityTxt.setText(quantity+"");
-//			}
-//		});
-//		final Button minButton = (Button) findViewById(R.id.min);
-//		minButton.setOnClickListener(new View.OnClickListener() {
-//
-//			@Override
-//			public void onClick(View v) {
-//				if(quantity==0){
-//
-//				}
-//				else{
-//					quantity = Double.parseDouble(quantityTxt.getText().toString());
-//					quantity--;
-//					quantityTxt.setText(quantity+"");
-//				}
-//
-//			}
-//		});
-//		final Button addButton = (Button) findViewById(R.id.addButton);
-//		addButton.setOnClickListener(new View.OnClickListener() {
-//
-//			@SuppressLint("NewApi")
-//			@Override
-//			public void onClick(View v) {
-//				boolean checkPrice = itemPrice.getText().toString().isEmpty();
-//				boolean checkBarcode = itemBarcode.getText().toString().isEmpty();
-//				if(checkPrice||checkBarcode){
-//					Toast.makeText(StockAddActivity.this,
-//							"Still have some Blank Field!", Toast.LENGTH_SHORT)
-//							.show();
-//				}
-//				else{
-//					Date now = new Date(); << DEPRECATED
-//					String time = new SimpleDateFormat("dd MMM yyyy").format(now);
-//					int id = inventory.getProductCatalog().getProductByBarcode(itemBarcode.getText().toString()).getId();
-//					boolean success = inventory.getStock().addProductLot(time, quantity, id,Double.parseDouble(itemPrice.getText().toString()));
-//					if(success){
-//						Toast.makeText(StockAddActivity.this,
-//								"Successfully Add : "+itemName.getText().toString(), Toast.LENGTH_SHORT)
-//								.show();
-//					}
-//					else{
-//						Toast.makeText(StockAddActivity.this,
-//								"FAIL to Add : "+itemName.getText().toString(), Toast.LENGTH_SHORT)
-//								.show();
-//					}
-//				}
-//			}
-//		});
-//
-//		final Button clearButton = (Button) findViewById(R.id.clearButton);
-//		clearButton.setOnClickListener(new View.OnClickListener() {
-//
-//			@Override
-//			public void onClick(View v) {
-//				quantity=0;
-//				itemBarcode.setText("");
-//				itemName.setText("");
-//				itemPrice.setText("");
-//				quantityTxt.setText(quantity+"");
-//
-//			}
-//		});
-//	}
+		
 		private void clearAllBox() {
 			costBox.setText("");
 			quantityBox.setText("");
