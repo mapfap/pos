7ae3f93e734fe62e4fd8a73eab9dd64452cd9f0a
mapfap <mapkabb@gmail.com>
2013-11-23 23:43:23 +0700

    Sale Report

--- src/com/refresh/pos/ui/ReportFragment.java
+++ src/com/refresh/pos/ui/ReportFragment.java
@@ -1,23 +1,27 @@
 package com.refresh.pos.ui;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import com.refresh.pos.R;
 import com.refresh.pos.database.NoDaoSetException;
 import com.refresh.pos.domain.Inventory;
+import com.refresh.pos.domain.Sale;
 import com.refresh.pos.domain.SaleLedger;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 
 public class ReportFragment extends Fragment {
@@ -57,4 +61,28 @@ public class ReportFragment extends Fragment {
 		});
 		
 	}
+	
+	private void showList(List<Sale> list) {
+
+		saleList = new ArrayList<Map<String, String>>();
+		for (Sale sale : list) {
+			saleList.add(sale.toMap());
+//			Log.d("ledger", sale.getStartTime());
+//			Log.d("ledger", sale.getEndTime());
+		}
+		
+		SimpleAdapter sAdap;
+		sAdap = new SimpleAdapter(getActivity().getBaseContext() , saleList,
+				R.layout.listview_saleledger, new String[] { "id", "startTime", "endTime", "status"},
+				new int[] { R.id.sid, R.id.startTime, R.id.endTime, R.id.status });
+		saleLedgerListView.setAdapter(sAdap);
+	}
+
+	@Override
+	public void onResume() {
+		Log.d("ledger",saleLedger.getAllSale().size()+"");
+		showList(saleLedger.getAllSale());
+		super.onResume();
+	}
+
 }
