52696869fc7af5e06116a5c3482ecfd6fe0b2653
teeravipark <teeravipark@gmail.com>
2013-11-26 00:15:42 +0700

    fix tab_detail , tab_stock , PDActivity

--- src/com/refresh/pos/ui/ProductDetailActivity.java
+++ src/com/refresh/pos/ui/ProductDetailActivity.java
@@ -6,17 +6,15 @@ import java.util.Map;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TabHost;
-import android.widget.TextView;
 
 import com.refresh.pos.R;
 import com.refresh.pos.database.NoDaoSetException;
@@ -28,82 +26,133 @@ import com.refresh.pos.domain.Stock;
 
 public class ProductDetailActivity extends Activity {
 
-        private ProductCatalog productCatalog;
-        private Stock stock;
-        private Product product;
-        private List<Map<String, String>> stockList;
-        private TextView nameBox;
-        private TextView barcodeBox;
-        private TextView priceBox;
-        private Button addProductLotButton;
-        private TabHost mTabHost;
-        private ListView stockListView;
-        private String id;
+	private ProductCatalog productCatalog;
+	private Stock stock;
+	private Product product;
+	private List<Map<String, String>> stockList;
+	private EditText nameBox;
+	private EditText barcodeBox;
+	private EditText priceBox;
+	private Button addProductLotButton;
+	private Button submitEditButton;
+	private TabHost mTabHost;
+	private ListView stockListView;
+	private String id;
 
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-        
-                try {
-                        stock = Inventory.getInstance().getStock();
-                        productCatalog = Inventory.getInstance().getProductCatalog();
-                } catch (NoDaoSetException e) {
-                        e.printStackTrace();
-                }
-                
-                Log.d("Product Detail", "id = " + getIntent().getStringExtra("id").toString());
-                id = getIntent().getStringExtra("id");
-                product = productCatalog.getProductById(Integer.parseInt(id));
-                
-                
-                initUI(savedInstanceState);
-                
-                
-                nameBox.setText(product.getName());
-                priceBox.setText(product.getUnitPrice()+"");
-                barcodeBox.setText(product.getBarcode());
-                
-        }
-        
-        private void initUI(Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                setContentView(R.layout.layout_productdetail);
-                stockListView = (ListView) findViewById(R.id.stockListView);
-                nameBox = (TextView) findViewById(R.id.nameBox);
-                priceBox = (TextView) findViewById(R.id.priceBox);
-                barcodeBox = (TextView) findViewById(R.id.barcodeBox);
-                addProductLotButton = (Button) findViewById(R.id.addProductLotButton);
-                mTabHost = (TabHost) findViewById(android.R.id.tabhost);
-                mTabHost.setup();
-                mTabHost.addTab(mTabHost.newTabSpec("tab_test1").setIndicator("Detail").setContent(R.id.tab1));
-                mTabHost.addTab(mTabHost.newTabSpec("tab_test2").setIndicator("Stock").setContent(R.id.tab2));
-                mTabHost.setCurrentTab(0);
-                
-                addProductLotButton.setOnClickListener(new View.OnClickListener() {
-                        public void onClick(View v) {
-                                Intent newActivity = new Intent(ProductDetailActivity.this, AddProductLotActivity.class);
-                                newActivity.putExtra("id", product.getId()+"");
-                                startActivity(newActivity);
-                        }
-                });
-        }
-        
-        private void showList(List<ProductLot> list) {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
 
-                stockList = new ArrayList<Map<String, String>>();
-                for(ProductLot productLot : list) {
-                        stockList.add(productLot.toMap());
-                }
+		try {
+			stock = Inventory.getInstance().getStock();
+			productCatalog = Inventory.getInstance().getProductCatalog();
+		} catch (NoDaoSetException e) {
+			e.printStackTrace();
+		}
 
-                SimpleAdapter sAdap;
-                sAdap = new SimpleAdapter(ProductDetailActivity.this, stockList,
-                                R.layout.listview_productstock, new String[]{"dateAdded","cost","quantity", "left"}, new int[] {R.id.dateAdded,R.id.cost,R.id.quantity,R.id.left});
-                stockListView.setAdapter(sAdap);
-        }
-        
-        @Override
-        protected void onResume() {
-                super.onResume();
-                showList(stock.getProductLotByProductId(Integer.parseInt(id)));
-        }
+		Log.d("Product Detail", "id = "
+				+ getIntent().getStringExtra("id").toString());
+		id = getIntent().getStringExtra("id");
+		product = productCatalog.getProductById(Integer.parseInt(id));
+
+		initUI(savedInstanceState);
+
+		nameBox.setText(product.getName());
+		priceBox.setText(product.getUnitPrice() + "");
+		barcodeBox.setText(product.getBarcode());
+
+	}
+
+	private void initUI(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.layout_productdetail);
+		stockListView = (ListView) findViewById(R.id.stockListView);
+		nameBox = (EditText) findViewById(R.id.nameBox);
+		priceBox = (EditText) findViewById(R.id.priceBox);
+		barcodeBox = (EditText) findViewById(R.id.barcodeBox);
+		submitEditButton = (Button) findViewById(R.id.submitEditButton);
+		addProductLotButton = (Button) findViewById(R.id.addProductLotButton);
+		mTabHost = (TabHost) findViewById(android.R.id.tabhost);
+		mTabHost.setup();
+		mTabHost.addTab(mTabHost.newTabSpec("tab_test1").setIndicator("Detail")
+				.setContent(R.id.tab1));
+		mTabHost.addTab(mTabHost.newTabSpec("tab_test2").setIndicator("Stock")
+				.setContent(R.id.tab2));
+		mTabHost.setCurrentTab(0);
+
+		addProductLotButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent newActivity = new Intent(ProductDetailActivity.this,
+						AddProductLotActivity.class);
+				newActivity.putExtra("id", product.getId() + "");
+				startActivity(newActivity);
+			}
+		});
+
+		nameBox.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				nameBox.setFocusable(true);
+				nameBox.setFocusableInTouchMode(true);
+				nameBox.setBackgroundColor(Color.parseColor("#C0FF3E"));
+			}
+		});
+
+		priceBox.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				priceBox.setFocusable(true);
+				priceBox.setFocusableInTouchMode(true);
+				priceBox.setBackgroundColor(Color.parseColor("#C0FF3E"));
+			}
+		});
+
+		barcodeBox.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				barcodeBox.setFocusable(true);
+				barcodeBox.setFocusableInTouchMode(true);
+				barcodeBox.setBackgroundColor(Color.parseColor("#C0FF3E"));
+			}
+		});
+
+		submitEditButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				nameBox.setFocusable(false);
+				nameBox.setFocusableInTouchMode(false);
+				nameBox.setBackgroundColor(Color.parseColor("#FFFFFF"));
+				priceBox.setFocusable(false);
+				priceBox.setFocusableInTouchMode(false);
+				priceBox.setBackgroundColor(Color.parseColor("#87CEEB"));
+				barcodeBox.setFocusable(false);
+				barcodeBox.setFocusableInTouchMode(false);
+				barcodeBox.setBackgroundColor(Color.parseColor("#87CEEB"));
+				product.setName(nameBox.getText().toString());
+				product.setUnitPrice(Double.parseDouble(priceBox.getText().toString()));
+				product.setBarcode(barcodeBox.getText().toString());
+				productCatalog.editProduct(product);
+			}
+		});
+	}
+
+	private void showList(List<ProductLot> list) {
+
+		stockList = new ArrayList<Map<String, String>>();
+		for (ProductLot productLot : list) {
+			stockList.add(productLot.toMap());
+		}
+
+		SimpleAdapter sAdap;
+		sAdap = new SimpleAdapter(ProductDetailActivity.this, stockList,
+				R.layout.listview_productstock, new String[] { "dateAdded",
+						"cost", "quantity", "left" }, new int[] {
+						R.id.dateAdded, R.id.cost, R.id.quantity, R.id.left });
+		stockListView.setAdapter(sAdap);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		showList(stock.getProductLotByProductId(Integer.parseInt(id)));
+	}
 
 }
