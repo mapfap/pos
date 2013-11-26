8544969d3ae20c527354f9c962c5773b5506c6e8
Natchanon Hongladaromp <natchanon.h@hotmail.com>
2013-11-26 16:28:53 +0700

    Try tab

--- src/com/refresh/pos/ui/MainActivity.java
+++ src/com/refresh/pos/ui/MainActivity.java
@@ -3,6 +3,11 @@ package com.refresh.pos.ui;
 import java.util.HashMap;
 import java.util.Map;
 
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.ActionBar.TabListener;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -10,6 +15,7 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.util.Log;
 import android.view.View;
 
@@ -25,17 +31,30 @@ import com.refresh.pos.domain.Inventory;
 import com.refresh.pos.domain.Register;
 import com.refresh.pos.domain.SaleLedger;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends FragmentActivity implements TabListener{
 
     private ViewPager viewPager;
+    private ActionBar actionBar;
 //    private Announcer productDetailAnnouncer;
 //    private Announcer saleAnnouncer;
     Map<String, Announcer> announcers;
     
-    @Override
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        
+        ActionBar.Tab tab1 = actionBar.newTab();
+        tab1.setText("Inventory");
+        tab1.setTabListener(this);
+        ActionBar.Tab tab2 = actionBar.newTab();
+        tab2.setText("Sale");
+        tab2.setTabListener(this);
+        ActionBar.Tab tab3 = actionBar.newTab();
+        tab3.setText("Report");
+        tab3.setTabListener(this);
         
         initiateCoreApp();
         
@@ -44,7 +63,27 @@ public class MainActivity extends FragmentActivity {
         announcers.put("Sale", new Announcer());
         viewPager= (ViewPager) findViewById(R.id.pager);
         FragmentManager fragmentManager = getSupportFragmentManager();
-        viewPager.setAdapter(new MyFragmentStatePagerAdapter(fragmentManager, announcers));
+        //viewPager.setAdapter(new MyFragmentStatePagerAdapter(getSupportFragmentManager(), announcers));
+        viewPager.setOnPageChangeListener(new OnPageChangeListener() {
+			
+			@Override
+			public void onPageSelected(int arg0) {
+				// TODO Auto-generated method stub
+				actionBar.setSelectedNavigationItem(arg0);
+			}
+			
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
         viewPager.setCurrentItem(1);
     }
     
@@ -86,6 +125,24 @@ public class MainActivity extends FragmentActivity {
 		DateTimeStrategy.setLocale("th", "TH");
 		
 		Log.d("Core App", "INITIATE");
+	}
+
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		viewPager.setCurrentItem(tab.getPosition());
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
