850b932acb620403eb6a34793766caf172d3b06e
LostSeaWay <LostSeaWay@me.com>
2013-11-27 14:36:12 +0700

    fix payment

--- src/com/refresh/pos/ui/sale/PaymentFragmentDialog.java
+++ src/com/refresh/pos/ui/sale/PaymentFragmentDialog.java
@@ -7,19 +7,29 @@ import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class PaymentFragmentDialog extends DialogFragment {
-
-	int totalPrice;
-//	public PaymentFragmentDialog(int totalPrice){
-//		this.totalPrice = totalPrice;
-//	}
 	
+	private TextView totalPrice;
+	private Button clearButton;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.dialog_payment, container,false);
-		
+		String strtext=getArguments().getString("edttext");
+		totalPrice = (TextView) v.findViewById(R.id.payment_total);
+		totalPrice.setText(strtext);
+		clearButton = (Button) v.findViewById(R.id.clearButton);
+		clearButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+			 
+				
+			}
+		});
 		return v;
 	}
 	
